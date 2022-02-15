857a5bc11f844edb0bd93daafe157e63acbd4fd3
aurakayla20 <3103120039@student.smktelkom-pwt.sch.id>
2022-02-15 09:41:33 +0700

    upload nodul 2

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+   // return view('welcome');
+//});
+
+Route::get('/', function() {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function() {
+    return view('about', [
+        "title" => "About",
+        "nama" => "Aura Kayla Chantika Prasasti",
+        "email" => "3103120039@student.smktelkom-pwt.sch.id",
+        "gambar" => "Aura.jpeg"
+    ]);
+});
+
+Route::get('/gallery', function() {
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
+});
